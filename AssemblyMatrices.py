da743fdea8d0fecc960cd3febd07f7be15f2caae
harel <harella1@hotmail.com>
2017-01-02 08:49:04 +0200

    1. good colors and titles 2. save to pdf 3. bad sizing

--- AssemblyMatrices.py
+++ AssemblyMatrices.py
@@ -1,8 +1,7 @@
 
 import clr
-import numpy as np
 import matplotlib
-matplotlib.use('PDF') 
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 clr.AddReference("System.Reflection")
@@ -36,12 +35,12 @@ for item in alltypes:
                 vars= [var.LocalType.Name for var in body.LocalVariables if var.LocalType and var.LocalType.FullName and 'System' not in var.LocalType.FullName]
                 d[key].Dependencies.extend([(method.Name,var)for var in vars])
 
-#pp = PdfPages('matrices.pdf')
-
 Interfaces = {}
 Dependencies = {}
 for pattern in d:
     x = d[pattern].Structors
+    if len(x) == 0:
+        continue
     y = list(set(d[pattern].Functionals))
     Interfaces[pattern] = np.zeros((len(x),len(y)), dtype=np.int)
     Dependencies[pattern] = np.zeros((len(x),len(y)), dtype=np.int)
@@ -51,14 +50,24 @@ for pattern in d:
         if method in y and item in x:
             Dependencies[pattern][x.index(item),y.index(method)] = 1
 
-    plt.figure()
-    plt.suptitle(pattern, fontsize=12)
-    plt.subplot(121)
-    plt.title('Interfaces')
-    plt.table(cellText=Interfaces[pattern], rowLabels=x, colLabels=y, loc=(0,0), cellLoc='center')
-    plt.subplot(122)
-    plt.title('Dependencies')
-    plt.table(cellText=Dependencies[pattern], rowLabels=x, colLabels=y,loc=(0,0), cellLoc='center')
+    fig = plt.figure(figsize = (len(x)+1,len(y)))
+    fig.suptitle(pattern, fontsize=18)           
+    gs = matplotlib.gridspec.GridSpec(nrows=2,ncols=1,left=0.3)
+
+    vals = Interfaces[pattern]*-100+300
+
+    ax1 = fig.add_subplot(gs[0])
+    ax1.axis('off')
+    ax1.set_title('Interfaces')
+    the_table = ax1.table(cellText=Interfaces[pattern], loc='center',cellLoc='center',rowLabels=x, colLabels=y,cellColours=plt.cm.hot(vals))
+    #the_table.scale(1.5, 1.5)
+
+    vals = Dependencies[pattern]*-100+300
+    ax2 = fig.add_subplot(gs[1])
+    ax2.axis('off')
+    ax2.set_title('Dependencies')
+    the_table = ax2.table(cellText=Dependencies[pattern], loc='center',cellLoc='center',rowLabels=x, colLabels=y,cellColours=plt.cm.hot(vals))
+    #the_table.scale(1.5, 1.5)
+    #fig.subplots_adjust(left=0.3),colWidths=[0.15]*len(y),colWidths=[0.15]*len(y)
     plt.savefig(pattern+'.pdf')
 
-#pp.close()
